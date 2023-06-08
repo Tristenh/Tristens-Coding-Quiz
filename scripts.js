8c8315a5d617caa2dd0e73ca89bf808bbae35793
Tristenh <tristenhobba1@gmail.com>
2023-06-08 22:43:38 +1000

    added multiple-choice article with buttons ,styled article and buttons, start button query selector with click event listener to run countdown function

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
+const timer = document.querySelector(".countdown");
+const start = document.querySelector(".start");
 
-let timer = document.querySelector(".countdown");
-countdown()
-function countdown(){
-    let timeLeft = 75
-    let timeInterval = setInterval(function(){
-        timeLeft --
-        timer.textContent = "Time : " + timeLeft
-        if(timeLeft === 0){
-            clearInterval(timeInterval)
-        }
-},1000);
+// start click event
+start.addEventListener("click", function () {
+  countdown();
+});
+
+// counting down function
+function countdown() {
+  let timeLeft = 75;
+  let timeInterval = setInterval(function () {
+    timeLeft--;
+    timer.textContent = "Time : " + timeLeft;
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 }
