0dce29b6d29b1faf4d4a95d9b42147f270d23578
Tristenh <tristenhobba1@gmail.com>
2023-06-09 21:49:07 +1000

    added hide contents function with display none. MultipleChoice article and endgame page, show question() display flex multiple-choice section

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
 const timer = document.querySelector(".countdown");
 const start = document.querySelector(".start");
 const multipleChoice = document.querySelectorAll(".multiple-choice");
-const correctAnswer = document.querySelectorAll(".correct");
+const correctAnswer = document.querySelector(".correct");
 const btn = document.querySelectorAll(".btn");
+const gameovver = document.querySelector(".gameover");
+const homepage = document.querySelector(".homepage");
 
 let timeLeft = 75;
 let points = 0;
-
+index = 0;
+// hide Content
+hideContent();
+function hideContent() {
+  for (let i = 0; i < multipleChoice.length; i++) {
+    multipleChoice[i].style.display = "none";
+    gameovver.style.display = "none";
+  }
+}
+// show questions
+function showQuestions() {
+  for (let i = 0; i < multipleChoice.length; i++) {
+    multipleChoice[i].style.display = "flex";
+  }
+}
 // add points
 for (let i = 0; i < correctAnswer.length; i++) {
   correctAnswer[i].addEventListener("click", function () {
@@ -14,21 +30,22 @@ for (let i = 0; i < correctAnswer.length; i++) {
     console.log(points);
   });
 }
-// wrong answer take saway 10 seconds
-minusTime()
+// wrong answer take away 10 seconds
+minusTime();
 function minusTime() {
   for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function () {
       points++;
       timeLeft -= 10;
-      console.log(points);
     });
   }
 }
 
 // start click event
 start.addEventListener("click", function () {
+  homepage.style.display = "none";
   countdown();
+  showQuestions(0);
 });
 
 // counting down function
@@ -36,8 +53,7 @@ function countdown() {
   let timeInterval = setInterval(function () {
     timeLeft--;
     timer.textContent = "Time : " + timeLeft;
-    if (timeLeft <= 0) {
-      minusTime();
+    if (timeLeft < 0 || timeLeft === 0) {
       clearInterval(timeInterval);
     }
   }, 1000);
