10836c90a45b5300937a8943a14993f39c1061b2
Tristenh <tristenhobba1@gmail.com>
2023-06-09 19:39:37 +1000

    added btn click event minus 10 seconds to timeLeft, btn correct changed to just correct in HTML, styles buttons alson includes correct class

--- scripts.js
+++ scripts.js
@@ -2,11 +2,24 @@ const timer = document.querySelector(".countdown");
 const start = document.querySelector(".start");
 const multipleChoice = document.querySelectorAll(".multiple-choice");
 const correctAnswer = document.querySelectorAll(".correct");
+const btn = document.querySelectorAll(".btn");
 
-let points = 1;
+let timeLeft = 75;
+let points = 0;
+
+// add points
 for (let i = 0; i < correctAnswer.length; i++) {
   correctAnswer[i].addEventListener("click", function () {
-    console.log(points++);
+    points++;
+    console.log(points);
+  });
+}
+// wrong answer take saway 10 seconds
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    points++;
+    timeLeft -= 10;
+    console.log(points);
   });
 }
 
@@ -17,7 +30,6 @@ start.addEventListener("click", function () {
 
 // counting down function
 function countdown() {
-  let timeLeft = 75;
   let timeInterval = setInterval(function () {
     timeLeft--;
     timer.textContent = "Time : " + timeLeft;
