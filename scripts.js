d491eaf7b9383210ea744f1f0017dd95a05b7644
Tristenh <tristenhobba1@gmail.com>
2023-06-10 12:13:39 +1000

    added answer-outcome div to HTML, styled answer-outcome,added showOutcome() iterates over answerOutcome uses ternary operator for correct or wrong fuction is then called with parameter true nextQuestion() and false for wrongQuestion()

--- scripts.js
+++ scripts.js
@@ -6,11 +6,25 @@ const btn = document.querySelectorAll(".btn");
 const gameover = document.querySelector(".gameover");
 const homepage = document.querySelector(".homepage");
 const score = document.querySelector(".score");
+const answerOutcome = document.querySelectorAll(".answer-outcome");
 
 let timeLeft = 75;
 let points = 0;
 let index = 0;
 gameover.style.display = "none";
+
+for (let i = 0; i < answerOutcome.length; i++) {
+  answerOutcome[i].style.display = "none";
+}
+function showOutcome(isCorrect) {
+  for (let i = 0; i < answerOutcome.length; i++) {
+    answerOutcome[i].style.display = "flex";
+    answerOutcome[i].textContent = isCorrect ? "correct" : "wrong";
+    setTimeout(function () {
+      answerOutcome[i].style.display = "none";
+    }, 1000);
+  }
+}
 
 // hide Content
 hideContent();
@@ -30,6 +44,7 @@ function nextQuestion() {
   for (let i = 0; i < correctAnswer.length; i++) {
     correctAnswer[i].addEventListener("click", function () {
       hideContent();
+      showOutcome(true);
       points++;
       index++;
       showQuestions(index);
@@ -37,10 +52,10 @@ function nextQuestion() {
   }
 }
 // wrong answer take away 10 seconds
-minusTime();
-function minusTime() {
+function wrongQuestion() {
   for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function () {
+      showOutcome(false);
       points++;
       timeLeft -= 10;
     });
@@ -53,6 +68,7 @@ start.addEventListener("click", function () {
   countdown();
   showQuestions(0);
   nextQuestion();
+  wrongQuestion();
 });
 
 // counting down function
@@ -70,6 +86,6 @@ function countdown() {
 
 function endGame() {
   hideContent();
-    gameover.style.display = "flex";
-    score.innerHTML = "Your final score is " + timeLeft
-  }
+  gameover.style.display = "flex";
+  score.innerHTML = "Your final score is " + timeLeft;
+}
